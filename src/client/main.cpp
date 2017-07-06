ea9c79537ea5413227b077793218a79c611b5508
yuri diego santos niitsuma <yuriniitsuma@mucuri.grad.dcc.ufmg.br>
2017-07-06 16:31:04 -0300

    step

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,60 +2,57 @@
 // https://gist.github.com/eduarc/6022859
 #include <bits/stdc++.h>
 
-#include "../common/utils.h"
+#include "common/utils.h"
 
 #include <sys/socket.h>
 
 #define PORT 8080
 
+class Client {
+private:
+  std::string host;
+  int port;
+
+  int sock;
+public:
+    Client(std::string host, int port){
+
+        if ((sock=socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+            std::cerr << FAIL << "Error in trying to opening socket" << ENDC <<std::endl;
+            exit(1);
+        }
+    };
+
+    ~Client(){
+        std::cout << WARNING << "Client died" << ENDC << std::endl;
+    };
+
+    void start();
+
+};
+
+void Client::start() {
+    
+}
+
 int main(int argc, char const *argv[])
 {
-	int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "Hello from server";
-      
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    std::vector<std::string> tokens;
+    if (argc < 2)
     {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+        std::cerr << FAIL << "Wrong number of argments" << std::endl;
+        std::cerr << "\tUSAGE: ./clientTP3 <IP:port>" << ENDC << std::endl;
+        exit(1);
     }
-      
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
+    else
     {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
+        tokens = utils::split_string(argv[1], ':');
     }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-      
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address, 
-                                 sizeof(address))<0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-                       (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+
+    // Create socket
+    Client client(tokens[0], stoi(tokens[1]));
+
+    client.start();
+
     return 0;
 }
