aa65f32a1d3eddd9ca0773cc592ea89387933c2a
Alison-Souza <alison.souza.mg@gmail.com>
2017-07-07 18:03:18 -0300

    Starting connections of servent

--- src/servent/main.cpp
+++ src/servent/main.cpp
@@ -32,6 +32,7 @@ int main(int argc, char *argv[])
 
 	unsigned int i;
 	int localport, socketFD;
+	bool find;
 	struct sockaddr_in servAddr;
 	string aux, line;
 	keyValue_t key;
@@ -56,8 +57,21 @@ int main(int argc, char *argv[])
 		getline(keyStream, aux);
 		key.value.append(aux);
 
-		// Insere o par chave-valor no dicionário.
-		dictionary.push_back(key);
+		// Atualiza dicionario se já existir entrada.
+		find = false;
+		for(i = 0; i < dictionary.size(); i++)
+		{
+			if(dictionary[i].name.compare(key.name) == 0)
+			{
+				dictionary[i].value = key.value;
+				find = true;
+				break;
+			}
+		}
+
+		// Insere no dicionário se ainda não existir entrada.
+		if(find == false)
+			dictionary.push_back(key);
 	}
 
 	i = argc;
@@ -71,6 +85,7 @@ int main(int argc, char *argv[])
 	}
 //--------------------------------------------------------------//
 	//PRINT PRA VER SE TÔ LENDO AS BUDEGA DIREITO.
+/*
 	std::cout << "DICTIONARY:" << endl;
 	for(i = 0; i < dictionary.size(); i++)
 		std::cout << dictionary[i].name << " - " << dictionary[i].value << endl;
@@ -78,11 +93,12 @@ int main(int argc, char *argv[])
 	std::cout << endl << "NEIGHBORHOOD:" << endl;
 	for(i = 0; i < neighborhood.size(); i++)
 		std::cout << neighborhood[i].ip << " - " << neighborhood[i].port << endl;
+*/
 //--------------------------------------------------------------//
 
 	if((socketFD = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
 	{
-		std::cerr << "Error in trying to opening socket. " << strerror(errno) << endl;
+		std::cerr << "Error on trying to opening socket. " << strerror(errno) << endl;
 		exit(1);
 	}
 	servAddr.sin_family = AF_INET;
@@ -93,6 +109,58 @@ int main(int argc, char *argv[])
 	{
 		std::cerr << "Error on trying to bind: " << strerror(errno) << endl;
 		exit(1);
+	}
+
+	std::cout << "Servent on port: " << localport << endl;
+	std::cout << "Waiting for data on UDP socket" << endl;
+
+	char* msg[100];
+	socklen_t cliLen;
+	struct sockaddr_in cliAddr;
+
+	// Loop de receber mensagens pelo servidor.
+	while(1)
+	{
+		// Limpando buffer que receberá a mensagem.
+		memset(msg, 0, 100);
+
+		// Recebendo a mensagem.
+		cliLen = sizeof(cliAddr);
+		if((recvfrom(socketFD, msg, 100, 0, (struct sockaddr*) &cliAddr, &cliLen)) < 0)
+		{
+			std::cerr << "Error on trying to receive data: " << strerror(errno) << endl;
+			continue;
+		}
+
+		// Imprimindo mensagem recebida.
+		std::cout << "Mensagem recebida: " << msg << endl;
+
+		// TODO: Conferir se está realmente recebendo corretamente.
+
+		/*
+		* TODO:
+		*
+		* Após receber a mensagem, identificar de qual tipo é a mensagem
+		* (CLIREQ ou QUERY) e fazer as ações necessárias.
+		*
+		* Se CLIREQ, criar uma QUERY e repassar. Após isso, verificar se
+		* o dicionário local tem a chave procurada, e se tiver responder
+		* para o cliente criando uma RESPONSE.
+		*
+		* Se QUERY, decrementar o TTL e repassar para os vizinhos menos
+		* de quem recebeu. Verificar se o dicionário local tem a chave
+		* procurada e se tiver, responder para o cliente com uma RESPONSE.
+		*
+		*/
+
+		/*
+		// Algo assim funcionaria ??
+		// Acho que sim, mas temos que definir o modelo das mensagens...
+		if(msg[0] == 1)
+			// mensagem do tipo CLIREQ
+		else if(msg[0] == 2)
+			// mensagem do tipo QUERY
+		*/
 	}
 
 	return 0;
