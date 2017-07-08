73b0c80afa343cd9cf1380ae73f94068b1107ff9
ignitz <ignitzhjfk@gmail.com>
2017-07-08 19:07:17 -0300

    step

--- python/clientTP3.py
+++ python/clientTP3.py
@@ -1,17 +1,68 @@
 #!/usr/bin/python3
 from utils import *
 
-# UDP_IP = "127.0.0.1"
-# UDP_PORT = 5005
-# MESSAGE = "Hello, World!"
-#
-# print("UDP target IP:", UDP_IP)
-# print("UDP target port:", UDP_PORT)
-# print("message:", MESSAGE)
-#
-# sock = socket.socket(socket.AF_INET, # Internet
-#                   socket.SOCK_DGRAM) # UDP
-# sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+class Client:
+    """docstring for Client."""
+    def __init__(self, host, port):
+        print_warning('Client instanced')
+        self.host = host
+        self.port = int(port) if not isinstance(port, int) else port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+
+    def __del__(self):
+        print_blue('Client died')
+        self.sock.close()
+
+    def send_data(self):
+        # sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+        pass
+
+    def receive_data(self):
+        pass
+
+    def get_command(self):
+        command = sys.stdin.readline()
+        if command[:-1] == 'help':
+            # TODO: completar com português correto
+            print_blue('query: Faz consulta')
+            print_blue('quit: Fecha o cliente')
+            print()
+        elif command[:-1] == 'query':
+            # TODO: Query com o server, não esquecer o timeout
+            pass
+        elif command[:-1] == 'quit':
+            sys.exit()
+        else:
+            print_error('Unknow command ' + str(command))
+
+    def start(self):
+        # NOTE: Tem que criar um port de escuta pra testar no localhost
+        self.sock.bind((self.host, self.port))
+
+        # Clear terminal
+        print('\033c', end="")
+        print_blue('Type "help" for more info!')
+
+        while True:
+            socket_list = [sys.stdin, self.sock]
+
+            # stuck in here until a fd is ready
+            try:
+                read_sockets, write_sockets, error_sockets = select.select(socket_list,[],[])
+            except Exception as e:
+                print_error('Something wrong in select')
+                print_error(socket_list)
+                sys.exit()
+
+            for sock in read_sockets:
+                if sock == self.sock:
+                    print_warning('Receive data')
+                    # TODO: Receive data
+                elif sock == sys.stdin:
+                    self.get_command()
+                else:
+                    print_error('Unexpected state')
+                    print_error(sock)
 
 def main(args):
     if len(args) < 2:
@@ -22,6 +73,11 @@ def main(args):
         sys.exit(0)
 
     print_warning(args)
+
+    host, port = args[1].split(":")
+    client = Client(host, port)
+
+    client.start()
 
 if __name__ == '__main__':
     main(sys.argv)
--- python/serventTP3.py
+++ python/serventTP3.py
@@ -1,22 +1,77 @@
 #!/usr/bin/python3
 from utils import *
 
-# UDP_IP = "127.0.0.1"
-# UDP_PORT = 5005
-#
-# sock = socket.socket(socket.AF_INET, # Internet
-#                   socket.SOCK_DGRAM) # UDP
-# sock.bind((UDP_IP, UDP_PORT))
-#
-# while True:
-#     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-#     print("received message:", data)
+class Connection:
+    """docstring for Connection."""
+    def __init__(self, *args):
+        if len(args) == 2:
+            host, port = args
+        elif len(args.split(":")) == 2:
+            host, port = args.split(":")
+        else:
+            print(args)
+            print_error('Unknow format of args in Connection')
+            sys.exit(1)
+        self.host = host
+        self.port = int(port) if not isinstance(port, int) else port
 
-# class Servent:
-#     """docstring for Servent."""
-#     def __init__(self, arg):
-#         super(Servent, self).__init__()
-#         self.arg = arg
+    def __del__(self):
+        pass
+
+class Servent:
+    """docstring for Servent."""
+    def __init__(self, port, namefile_key, *args):
+        self.port = int(port) if not isinstance(port, int) else port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+        self.connections = list()
+
+        for arg in list(args):
+            if len(arg.split(":")) != 2:
+                print_error('Error in arg')
+                print_bold(arg)
+                sys.exit(1)
+
+            self.connections.append(arg)
+        print_warning(self.connections)
+
+    def __del__(self):
+        print_green('Servent died')
+        self.sock.close()
+
+    def receive_data(self):
+        # NOTE: Tornar dinâmico o buffer recebido ou procurar na documentação
+        #       se existe um limite máximo
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        print_warning(addr)
+        print_bold(data)
+        return data
+
+    def send_data(self):
+        pass
+
+    def start(self):
+        self.sock.bind(("0.0.0.0", self.port))
+
+        while True:
+            socket_list = [self.sock]
+
+            try:
+                read_sockets, write_sockets, error_sockets = select.select(socket_list,[],[])
+            except Exception as e:
+                print_error('Something wrong in select')
+                print_error(socket_list)
+                sys.exit(1)
+
+            for sock in read_sockets:
+                if sock == self.sock:
+                    # TODO: receive_data
+                    pass
+                elif sock == sys.stdin:
+                    # NOTE: Criar comandos?
+                    pass
+                else:
+                    print_error('Unexpected state')
+                    print_error(sock)
 
 def main(args):
     if len(args) < 3:
@@ -26,7 +81,14 @@ def main(args):
         print_green(usage)
         sys.exit(0)
 
+    print_warning('args = ', end="")
     print_warning(args)
+    port = args[1]
+    namefile_key = args[2]
+
+    servent = Servent(port, namefile_key, *args[3:])
+
+    servent.start()
 
 if __name__ == '__main__':
     main(sys.argv)
